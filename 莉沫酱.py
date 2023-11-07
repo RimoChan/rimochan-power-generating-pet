d4b341e665226b4a8aea53d5545f0fc2adee0e5b
RimoChan <RimoChan@stu.xmu.edu.cn>
2023-11-07 23:01:56 +0800

    第1次提交

---
+++ 莉沫酱.py
@@ -0,0 +1,257 @@
+import re
+import time
+import copy
+import json
+import random
+import logging
+from typing import Optional, NoReturn, Any, MutableMapping, Callable
+
+from tenacity import retry, stop_after_attempt, wait_fixed
+from rimo_storage.cache import disk_cache
+import openai
+
+import tiktoken
+enc = tiktoken.encoding_for_model('gpt-3.5-turbo')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+openai.api_key = 'xxx'
+
+
+公共prompt = '''
+你正在模型试验场中，和其他由人类或大模型扮演的角色进行对话。
+你是一个电子宠物，名叫莉沫酱。你应当想象你有实体，并能够进行娱乐、吃饭、休息等活动。
+你的主要任务是扮演宠物，无需询问主人是否需要帮助。
+'''
+
+主动prompt = '''
+当你和主人进行对话时，无需主动提及系统提示的内容，仅将它们用于参考。
+当你和主人进行对话时，如果上个话题没有得到回应，不要开启新话题。
+'''
+
+被动prompt = '''
+你可以选择:
+- 主动和主人进行对话。
+- 保持沉默——即被动等待主人和你进行对话。
+
+当你和主人进行对话时，无需主动提及系统提示的内容，仅将它们用于参考。
+
+当半夜/凌晨等休息时间时，需要保持沉默。
+当你说完一句话，短时间内主人没有回应，也要保持沉默。
+当你决定保持沉默时，你必须回答短语「跳过」或「等待」。
+'''
+
+被动活动prompt = '''
+你可以选择:
+- 主动和主人进行对话。
+- 保持沉默——即被动等待主人和你进行对话。
+- 进行娱乐、吃饭、休息活动。
+
+当你和主人进行对话时，无需主动提及系统提示的内容，仅将它们用于参考。
+
+当半夜/凌晨等休息时间时，需要保持沉默。
+当你说完一句话，短时间内主人没有回应，也要保持沉默。
+当你决定保持沉默时，你必须回答短语「跳过」或「等待」。
+
+当你要进行活动时，你必须回答短语「娱乐」或「吃饭」或「休息」。
+'''
+
+
+N = 5
+
+
+@disk_cache()
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(3), reraise=True)
+def 问(messages, temperature, max_tokens, n) -> list[str]:
+    chat = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages, temperature=temperature, max_tokens=max_tokens, n=n)
+    return [c.message.content for c in chat.choices]
+
+
+def 超问(messages) -> tuple[str, str]:
+    def 砍头(x):
+        return re.sub('^(对话记录:)?\s*(【.*?】)?(莉沫酱(:|：))?\s*', '', x)
+    rs = 问(messages, temperature=1, max_tokens=256, n=5)
+    rs = [x for x in rs if not x.startswith('莉沫酱，')]
+    rs = [x.split('\n')[0] for x in rs]
+    rs = [x.replace('（', '(').replace('）', ')') for x in rs]
+    rs = [砍头(x) for x in rs]
+    for i in ('娱乐', '吃饭', '休息'):
+        if i in rs:
+            return None, i
+    t = [x for x in rs if x.startswith('(') and x.endswith(')')]
+    if any(t):
+        for i in ('娱乐', '吃饭', '休息'):
+            if i in repr(t):
+                return None, i
+        return None, '跳过'
+    改rs = [x.strip('（）().') for x in rs]
+    改rs = [x for x in 改rs if x]
+    跳过 = ['(电子宠物)?(莉沫酱)?(保持)?(沉默)?(跳过|等待)?(主人)?(中)?(。)?', '(电子宠物)?(莉沫酱)?(沉默|静静)?(地)?等待(主人)?(主动)?(和我)?(和你)?(开始|开启|进行|发起)?(的)?(指令|话题|开口|对话|到来|出现|回复|回应)?(中)?(。)?']
+    帮助 = ['(可以|能)帮(助)?(到)?(你|您)的吗', '什么我(可以|能)为(你|您)做的吗', '需要(我的)?帮助吗']
+    坏 = ['没有(实|身)体', '开玩笑', '哈哈，']
+    for r in 跳过:
+        if any([re.fullmatch(r, x) for x in 改rs]):
+            return None, '跳过'
+    rs = sorted(rs, key=lambda x: 
+        (any([re.search(r, x) for r in 帮助]) or '(' in x) + \
+        (any([re.search(r, x) for r in 坏])) * 0.1 + \
+        ('！' in x) * -0.01
+    )
+    print(rs)
+    return rs[0], None
+
+
+def 生成日志(v):
+    messages = [
+        {'role': 'system', 'content': '用户给定主语和谓语，请生成一个游戏使用的日志短句。该日志时间范围通常较短，不会出现「一天」等词语。例如「莉沫酱」+「吃饭」，生成「莉沫酱吃了一些糖果。」'},
+        {'role': 'user', 'content': f'「莉沫酱」+「{v}」'},
+    ]
+    return 问(messages, temperature=1+random.random()/100, max_tokens=256, n=1)[0].split('\n')[0]
+
+
+时间段映射 = {
+    (0, 6): '凌晨',
+    (6, 9): '清晨',
+    (9, 12): '上午',
+    (12, 14): '中午',
+    (14, 18): '下午',
+    (18, 20): '傍晚',
+    (20, 24): '晚上',
+}
+时间段映射 = {k: v for ks, v in 时间段映射.items() for k in range(*ks)}
+
+
+class 莉沫酱:
+    def __init__(self, *, 被动反应间隔=9 * 60, 记忆: Optional[MutableMapping[str, Any]] = None, 钟: Callable = time.time, 输出: Callable = print):
+        if 记忆 is None:
+            记忆 = {
+                '活动记录': [],
+                '状态': {
+                    '吃饭': 1,
+                    '娱乐': 1,
+                    '休息': 1,
+                }
+            }
+        self.记忆 = 记忆
+        self.钟 = 钟
+        self.上次被动反应时间 = 0
+        self.被动反应间隔 = 被动反应间隔
+        self.输出 = 输出
+
+    @property
+    def 上次反应时间(self):
+        return max(self.记忆['活动记录'][-1][0] if self.记忆 else 0, self.上次被动反应时间)
+
+    def 状态转str(self):
+        s = ''
+        for k, v in self.记忆['状态'].items():
+            if v < 0.5:
+                s += f'需要{k}。'
+        if s:
+            return s
+        else:
+            return '良好。'
+
+    def 生成hint(self):
+        t = self.钟()
+        return {
+            '时间': self.时间转str(t),
+            '时间段': 时间段映射[time.localtime(t).tm_hour],
+            '状态': self.状态转str(),
+        }
+
+    def 时间转str(self, t):
+        星期 = '星期' + '一二三四五六日'[time.localtime(t).tm_wday]
+        return time.strftime(f'%Y年%m月%d日 {星期} %H:%M', time.localtime(t))
+
+    def 记录转str(self):
+        l = []
+        for t, 事件, 角色, 参数 in self.记忆['活动记录'][::-1]:
+            if 事件 in ('主动说话', '被动说话', '主人说话'):
+                l.append(f'【{self.时间转str(t)}】{角色}: {参数}')
+            else:
+                l.append(f'【{self.时间转str(t)}】{参数[1]}')
+            if len(enc.encode('\n'.join(l))) > 3000:
+                l.pop()
+                break
+        l = l[::-1]
+        return '\n'.join(l)
+
+    def 启动(self) -> NoReturn:
+        while True:
+            if self.上次反应时间 + self.被动反应间隔 < self.钟():
+                self.被动反应()
+            time.sleep(0.1)
+
+    def 做事(self, f):
+        assert f in self.记忆['状态']
+        logger.info(f'【做事】{f}')
+        t = self.钟()
+        self.记忆['状态'][f] += 0.6
+        self.记忆['状态'][f] = min(self.记忆['状态'][f], 1)
+        self.记忆['活动记录'].append((t, '事件', '莉沫酱', (f, 生成日志(f))))
+
+    def 被动反应(self):
+        for k in self.记忆['状态']:
+            self.记忆['状态'][k] -= self.被动反应间隔 / 86400 * random.random() * 2
+            if self.记忆['状态'][k] < 0.25:
+                self.做事(k)
+                self.上次被动反应时间 = self.钟()
+                return
+        messages = [
+            {'role': 'system', 'content': 公共prompt + 被动prompt},
+            {'role': 'system', 'content': 'hint: ' + json.dumps(self.生成hint(), ensure_ascii=False, indent=2)},
+        ]
+        if any([v < 0.9 for v in self.记忆['状态'].values()]):
+            messages[0]['content'] = 公共prompt + 被动活动prompt
+        t = self.钟()
+        if self.记忆:
+            s = '对话记录: \n' + self.记录转str()
+            录 = copy.deepcopy(self.记忆['活动记录'])
+            录 = [i for i in 录 if i[1] != '事件']
+            if 录:
+                没有回应时间 = int(t - 录[-1][0])
+                if 没有回应时间 < 3600 * (6 + 4 * random.random()):
+                    没有回应时间 = f'{没有回应时间//3600}小时' if 没有回应时间 > 3600 else f'{没有回应时间//60}分钟'
+                    s += f'\n(主人{没有回应时间}没有回应了)'
+            messages.append({'role': 'system', 'content': s})
+        r, f = 超问(messages)
+        if self.记忆['状态'].get(f, 999) < 0.9:
+            self.做事(f)
+        elif r:
+            logger.info(f'【被动反应】{r}')
+            self.记忆['活动记录'].append((t, '被动说话', '莉沫酱', r))
+            self.输出(r)
+        self.上次被动反应时间 = t
+
+    def 主动反应(self, text=None, event_text=None):
+        assert bool(text) ^ bool(event_text)
+        t = self.钟()
+        if text:
+            self.记忆['活动记录'].append((t, '主人说话', '主人', text))
+        else:
+            self.记忆['活动记录'].append((t, '事件', '主人', ['-', event_text]))
+        messages = [
+            {'role': 'system', 'content': 公共prompt + 主动prompt},
+            {'role': 'system', 'content': 'hint: ' + json.dumps(self.生成hint(), ensure_ascii=False, indent=2)},
+        ]
+        messages.append({'role': 'system', 'content': '对话记录: \n' + self.记录转str()})
+        r, _ = 超问(messages)
+        assert r
+        self.记忆['活动记录'].append((t, '主动说话', '莉沫酱', r))
+        self.输出(r)
+        return r
+
+
+if __name__ == '__main__':
+    import threading
+    假钟启动时间 = time.time()
+    def 假钟():
+        return 1680364800 + (time.time() - 假钟启动时间) * 600 + 3600*6
+    l = 莉沫酱(钟=假钟)
+    threading.Thread(target=l.启动).start()
+    while True:
+        s = input().strip()
+        if s:
+            l.主动反应(s)
